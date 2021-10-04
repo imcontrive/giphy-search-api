e43cc26d3b6d021049910587ca6456d608aac9ec
Shubham Pandey <shubham.pandey@fangame.live>
2021-10-05 01:35:40 +0530

    added App.js and giphy.css

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Giphy from './components/Giphy';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="heading">Giphy API Search</h2>
+      <Giphy/>
+    </div>
+  );
+}
+
+export default App;
